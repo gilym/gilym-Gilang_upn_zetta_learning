38fb1bc3b3493e3399d72483d2183bf641fb456b
Gilang Yoenal Marinta <gilym46@gmail.com>
2022-10-24 19:47:49 +0900

    commit

---
+++ JsDay1/Script.js
@@ -0,0 +1,36 @@
+
+function price (book,discount,tax){
+    let result=0;
+
+    if(book && book.price){
+        const price = book.price;
+        const amountdisc= price * (discount/100);
+        const pricedisc = price - amountdisc;
+        const amounttax = pricedisc * (tax/100);
+        const pricetax = pricedisc - amounttax;
+
+        console.group();
+        console.log('TItle                  :' ,book.title);
+        console.log('Original Price         :' ,price);
+        console.log('Discount Amount        :' ,amountdisc);
+        console.log('Price After Discount   :' ,pricedisc);
+        console.log('Tax Amount             :' ,amounttax);
+        console.log('Price After Tax        :' ,pricetax);
+        console.groupEnd();
+
+        return result;
+
+    }
+
+    price({title : 'Aku dan Dia' , price:10000 ,printing_status :true} ,50,10);
+   
+
+   
+
+    
+
+}
+
+console.log(price(10000,10,5));
+
+
