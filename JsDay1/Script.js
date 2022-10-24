43fd52c51951bbde754aa068d0f615178ab32eb2
Gilang Yoenal Marinta <gilym46@gmail.com>
2022-10-25 02:40:49 +0900

    commit

--- JsDay1/Script.js
+++ JsDay1/Script.js
@@ -1,36 +1,56 @@
+/***JS Day 2
+ * Update From JsDay1
+ * 
+ */
 
-function price (book,discount,tax){
+function price (book,discount,tax,stock,purchased){
     let result=0;
+    let harga,disc,totaldisc,taxes,totaltax
 
-    if(book && book.price){
-        const price = book.price;
-        const amountdisc= price * (discount/100);
-        const pricedisc = price - amountdisc;
-        const amounttax = pricedisc * (tax/100);
-        const pricetax = pricedisc - amounttax;
+    for(let i=0 ; i<purchased ; i++){
+       
+        if(book && book.price &&( i<stock)){
+         
+            const price = book.price;
+            const amountdisc= price * (discount/100);
+            const pricedisc = price - amountdisc;
+            const amounttax = pricedisc * (tax/100);
+            const pricetax = pricedisc - amounttax;
+            result+=pricetax;
+            
 
-        console.group();
-        console.log('TItle                  :' ,book.title);
-        console.log('Original Price         :' ,price);
-        console.log('Discount Amount        :' ,amountdisc);
-        console.log('Price After Discount   :' ,pricedisc);
-        console.log('Tax Amount             :' ,amounttax);
-        console.log('Price After Tax        :' ,pricetax);
-        console.groupEnd();
-
-        return result;
-
+           harga =price;
+           disc=amountdisc;
+           totaldisc=pricedisc;
+           taxes=amounttax;
+           totaltax=pricetax;
+         
+    
+        }
+        else{
+            console.log("Stok Habis");
+            break;
+        }
+       
+      
+                                                              
+     
     }
 
-    price({title : 'Aku dan Dia' , price:10000 ,printing_status :true} ,50,10);
-   
+    console.group();
+    console.log('Title                  :' ,book.title);
+    console.log('Original Price         :' ,harga);
+    console.log('Discount Amount        :' ,disc);
+    console.log('Price After Discount   :' ,totaldisc);
+    console.log('Tax Amount             :' ,taxes);
+    console.log('Price After Tax        :' ,totaltax);
+    console.log('Total Order            :' ,purchased);
+    console.log('Total                  :' ,result);
+    console.groupEnd();
 
-   
-
+    return 0;
     
-
 }
-
-console.log(price(10000,10,5));
+price({title : 'Aku dan Dia' , price:10000 ,printing_status :true} ,50,10,20,10);
 
 
